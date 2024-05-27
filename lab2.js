cd50260f0b818dfca2aeaaff82476f9da516586c
ifatimahali <star18salfa@gmail.com>
2024-05-27 19:29:01 +0300

    f

---
+++ lab2.js
@@ -0,0 +1,20 @@
+let screenTv = document.getElementById("screen");
+let channels = document.querySelectorAll(".channel")
+
+let img = [
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg', 
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg',
+'https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg'
+];
+
+for (let index=0; index < img.length; index++ ){
+    channels[index].addEventListener("click", () => {
+        screenTv.src=img[index]
+    });
+}
